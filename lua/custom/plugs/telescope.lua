85c45bdf1161fc524032a5ad80ce6aba7c8fe192
bashNeko <bashnko@users.noreply.github.com>
2026-05-08 19:51:00 +0530

    feat(telescope): add fzf extension and config

--- lua/custom/plugs/telescope.lua
+++ lua/custom/plugs/telescope.lua
@@ -1,3 +1,6 @@
+vim.pack.add({
+	{ src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
+})
 require("telescope").setup({
 	defaults = {
 		borderchars = {
@@ -18,15 +21,30 @@ require("telescope").setup({
 	},
 	pickers = {
 		find_files = {
-			hidden = false,
+			theme = "dropdown",
+			find_command = { "fd", "--type", "f", "--hidden", "--follow", "--exclude", ".git" },
+		},
+		live_grep = {},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
 		},
 	},
 })
 
 local builtin = require("telescope.builtin")
+pcall(function()
+	require("telescope").load_extension("fzf")
+end)
 vim.keymap.set("n", "<leader>st", builtin.find_files, {})
 vim.keymap.set("n", "<leader>cm", builtin.git_commits, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ss", builtin.live_grep, { silent = true })
 vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
+vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
+vim.keymap.set("n", "<leader>fn", builtin.lsp_document_symbols, {})
